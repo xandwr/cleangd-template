456d05d239b8f93fcb05916668eec6b858a5ce09
Xander Pickering <ajamespickering@gmail.com>
2026-06-08 18:46:32 -0600

    Organization

--- scenes/player/PlayerBody.cs
+++ scenes/player/PlayerBody.cs
@@ -8,101 +8,65 @@ namespace Xandbox.Player;
 [GlobalClass]
 public partial class PlayerBody : CharacterBody3D
 {
-    /// Spring-arm length per view: collapsed onto the head for first-person,
-    /// pulled back behind the body for third-person.
-    public enum CameraPerspective { FirstPerson, ThirdPerson }
+	[Export]
+	public float Speed = 3.5f;
 
-    private CameraPerspective _perspective = CameraPerspective.FirstPerson;
+	[Export]
+	public float SprintSpeed = 6.5f;
 
-    [Export]
-    public CameraPerspective Perspective
-    {
-        get => _perspective;
-        set
-        {
-            _perspective = value;
-            ApplyPerspective();
-        }
-    }
+	[Export]
+	public float JumpSpeed = 6.0f;
 
-    /// Distance the arm pulls back in third-person.
-    [Export]
-    public float ThirdPersonDistance = 3.0f;
+	[Export(PropertyHint.Range, "0.0001,0.01")]
+	public float MouseSensitivity = 0.002f;
 
-    [Export]
-    public float Speed = 5.0f;
+	private SpringArm3D _cameraArm;
+	private float _gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
 
-    [Export]
-    public float SprintSpeed = 8.0f;
+	public override void _Ready()
+	{
+		_cameraArm = GetNode<SpringArm3D>("%CameraArm");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
 
-    [Export]
-    public float JumpSpeed = 8.0f;
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion motion && Input.MouseMode == Input.MouseModeEnum.Captured)
+		{
+			RotateY(-motion.Relative.X * MouseSensitivity);
 
-    [Export(PropertyHint.Range, "0.0001,0.01")]
-    public float MouseSensitivity = 0.003f;
+			var pitch = _cameraArm.Rotation;
+			pitch.X = Mathf.Clamp(
+				pitch.X - motion.Relative.Y * MouseSensitivity,
+				Mathf.DegToRad(-89.0f),
+				Mathf.DegToRad(89.0f));
+			_cameraArm.Rotation = pitch;
+		}
 
-    private SpringArm3D _cameraArm;
-    private float _gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
+		if (Input.IsActionJustPressed("pause"))
+			Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured
+				? Input.MouseModeEnum.Visible
+				: Input.MouseModeEnum.Captured;
+	}
 
-    public override void _Ready()
-    {
-        _cameraArm = GetNode<SpringArm3D>("%CameraArm");
-        ApplyPerspective();
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		var velocity = Velocity;
 
-    private void ApplyPerspective()
-    {
-        if (_cameraArm == null)
-            return;
-        _cameraArm.SpringLength = _perspective == CameraPerspective.ThirdPerson
-            ? ThirdPersonDistance
-            : 0.0f;
-    }
+		if (!IsOnFloor())
+			velocity.Y -= _gravity * (float)delta;
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event is InputEventMouseMotion motion && Input.MouseMode == Input.MouseModeEnum.Captured)
-        {
-            RotateY(-motion.Relative.X * MouseSensitivity);
+		if (Input.IsActionJustPressed("move_jump") && IsOnFloor())
+			velocity.Y = JumpSpeed;
 
-            var pitch = _cameraArm.Rotation;
-            pitch.X = Mathf.Clamp(
-                pitch.X - motion.Relative.Y * MouseSensitivity,
-                Mathf.DegToRad(-89.0f),
-                Mathf.DegToRad(89.0f));
-            _cameraArm.Rotation = pitch;
-        }
+		var input = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+		var direction = (Transform.Basis * new Vector3(input.X, 0.0f, input.Y)).Normalized();
+		var speed = Input.IsActionPressed("move_sprint") ? SprintSpeed : Speed;
 
-        if (Input.IsActionJustPressed("pause"))
-            Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured
-                ? Input.MouseModeEnum.Visible
-                : Input.MouseModeEnum.Captured;
+		velocity.X = direction.X * speed;
+		velocity.Z = direction.Z * speed;
 
-        if (Input.IsActionJustPressed("toggle_perspective"))
-            Perspective = _perspective == CameraPerspective.FirstPerson
-                ? CameraPerspective.ThirdPerson
-                : CameraPerspective.FirstPerson;
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        var velocity = Velocity;
-
-        if (!IsOnFloor())
-            velocity.Y -= _gravity * (float)delta;
-
-        if (Input.IsActionJustPressed("move_jump") && IsOnFloor())
-            velocity.Y = JumpSpeed;
-
-        var input = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
-        var direction = (Transform.Basis * new Vector3(input.X, 0.0f, input.Y)).Normalized();
-        var speed = Input.IsActionPressed("move_sprint") ? SprintSpeed : Speed;
-
-        velocity.X = direction.X * speed;
-        velocity.Z = direction.Z * speed;
-
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
